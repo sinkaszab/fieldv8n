493059db226adfc58570ef099cc083c3c5e1f920
Szabolcs Sinka <sasi.sinka@icloud.com>
2020-04-07 01:15:14 +0200

    Reconfigure rollup to compile the complete bundle.

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,26 @@
-import typescript from "@rollup/plugin-typescript";
+import sucrase from "@rollup/plugin-sucrase";
+import resolve from "@rollup/plugin-node-resolve";
+import pkg from "./package.json";
 
 export default {
   input: "src/fieldv8n.ts",
-  output: {
-    dir: "dist",
-    format: "cjs",
-  },
-  plugins: [typescript()],
+  output: [
+    { file: pkg.main, format: "cjs", exports: "named" },
+    { file: pkg.module, format: "es" },
+    {
+      name: "fieldv8n",
+      file: pkg.browser,
+      format: "umd",
+      exports: "named",
+    },
+  ],
+  plugins: [
+    resolve({
+      extensions: [".js", ".ts"],
+    }),
+    sucrase({
+      exclude: ["node_modules/**"],
+      transforms: ["typescript"],
+    }),
+  ],
 };
