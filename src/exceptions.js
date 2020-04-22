f67825ac83f93c65c845d154d531153b225ade18
Szabolcs Sinka <sasi.sinka@icloud.com>
2020-04-22 16:07:48 +0200

    Remove all .js files.

--- src/exceptions.js
+++
@@ -1,9 +0,0 @@
-class InvalidData extends Error {}
-
-class ValidatorNameExists extends Error {}
-
-class TypeExists extends Error {}
-
-class ValidatorRuntimeError extends Error {}
-
-export { InvalidData, ValidatorNameExists, TypeExists, ValidatorRuntimeError };
--- src/functionalHelper.js
+++
@@ -1,4 +0,0 @@
-const asyncCompose = (...fns) => x =>
-  fns.reduceRight(async (y, f) => f(await y), x);
-
-export { asyncCompose };
